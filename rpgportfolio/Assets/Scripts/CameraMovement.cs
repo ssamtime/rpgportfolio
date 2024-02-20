4cf130dc4046b196d9ee52a972b3f3b30d43acca
JHP\lucky <lucky2489@naver.com>
2024-02-20 09:33:07 +0900

    asd

---
+++ rpgportfolio/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform objectTofollow;
+    public float followspeed =10f;
+    public float sensitivity = 100f;
+    public float clampAngle = 70f;
+    private float rotX;
+    private float rotY;
+    public Transform realCamera;
+    public Vector3 dirNormalized;
+    public Vector3 finalDir;
+    public float minDistance;
+    public float maxDistance;
+    public float finalDistance;
+    public float smoothness = 10f;
+
+
+    void Start()
+    {
+        // 변수 초기화
+        rotX = transform.localRotation.eulerAngles.x;
+        rotY = transform.localRotation.eulerAngles.y;
+        dirNormalized = realCamera.localPosition.normalized;
+        finalDistance = realCamera.localPosition.magnitude;
+
+        // 커서 안보이게
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+        rotX += -1 * Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        rotY += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        Quaternion rot = Quaternion.Euler(rotX, rotY, 0);
+        transform.rotation = rot;
+    }
+    private void LateUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+            objectTofollow.position, followspeed * Time.deltaTime);
+        finalDir = transform.TransformPoint(dirNormalized * maxDistance);
+
+        RaycastHit hit;
+
+        if(Physics.Linecast(transform.position,finalDir,out hit))
+        {
+            finalDistance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+        }
+        else
+        {
+            finalDistance = maxDistance;
+        }
+
+        realCamera.localPosition = Vector3.Lerp(realCamera.localPosition,
+            dirNormalized * finalDistance, Time.deltaTime * smoothness);
+    }
+
+}
